a6243a19896247630b117ce26d76d1c66d2ba94f
rojon <rjrojon@gmail.com>
2021-07-11 07:59:08 +0600

    prduct collection for different category

--- src/components/ProductCollectionPreview.jsx
+++ src/components/ProductCollectionPreview.jsx
@@ -1,37 +1,23 @@
-import ProductItem from "./ProductItem"
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import ProductItem from "./ProductItem";
 
 const ProductCollectionPreview = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('/products')
+          .then(res => setProducts(res.data))
+          .catch(err => console.log(err))
+    }, [])
+    
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 px-3">
-            
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
-            <ProductItem />
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-3 p-3 bg-gray-100">
+          {
+            products.map(product => <ProductItem key={product._id} product={product} />)
+          }
         </div>
     );
 };
